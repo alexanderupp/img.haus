1fd3e8f7c37ccadabecbb84db014dd254b645f2a
Alex Rupp <alexrupp@outlook.com>
2025-02-16 15:15:02 -0500

    fixed typo

--- views/home.php
+++ views/home.php
@@ -33,7 +33,7 @@
 						<li>DO upload images you have permission to use. This means images you have created or been given permission by the original creator.</li>
 						<li>DO NOT use us to host your website media content. Reddit/forum posts are allowed.</li>
 						<li>DO NOT upload gore, or material that is threatening, harassing, defamatory, or that encourages violence or crime, or in any way could be considered hate speech.</li>
-						<li>DO NOT upload "NSFW" content, such nudity, suggestive or adult content.</li>
+						<li>DO NOT upload "NSFW" content, such nudity, suggestive, or adult content.</li>
 						<li>DO NOT upload illegal content such as child porn or nonconsensual/revenge porn.</li>
 					</ul>
 				</section>
